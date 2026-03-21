5e2345d69a5686c82288ec4d08e142708dea7ad7
BerrioA <ingalejandroberrio@gmail.com>
2026-03-20 22:58:38 -0500

    Correcio de compatibilidad con servidores

---
+++ src/config/db.js
@@ -0,0 +1,24 @@
+import { Sequelize } from "sequelize";
+import { POSTGRES_URL, NODE_ENV } from "./env.js";
+
+const isProduction = NODE_ENV !== "development";
+
+const sequelizeOptions = {
+  dialect: "postgres",
+  protocol: "postgres",
+  logging: false,
+  // Evitar problemas de compatibilidad con algunos entornos de producción
+  native: false,
+};
+
+// Añadir SSL solo en producción
+if (isProduction) {
+  sequelizeOptions.dialectOptions = {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  };
+}
+
+export const sequelize = new Sequelize(POSTGRES_URL, sequelizeOptions);
